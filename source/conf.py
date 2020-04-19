da6721400ffb0cce3796d254c8a117308a582afb
Mariatta <Mariatta@users.noreply.github.com>
2020-04-19 13:11:29 -0700

    Move to docs directory (#1)
    * Move to docs directory
    
    * Add sphinx to requirements
    
    * Fix the configuration

--- source/conf.py
+++ source/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Say It With Bots!'
-copyright = '2020, Mariatta Wijaya'
-author = 'Mariatta Wijaya'
+project = "Say It With Bots!"
+copyright = "2020, Mariatta Wijaya"
+author = "Mariatta Wijaya"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,21 +27,15 @@ author = 'Mariatta Wijaya'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-]
+extensions = ["sphinx.ext.intersphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# The master toctree document.
-master_doc = 'readme'
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*venv*']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,33 +43,33 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*venv*']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ["_static"]
 
 html_theme_options = {
-    'github_user': 'mariatta',
-    'github_repo': 'github-app-tutorial',
-    'github_type': 'star',
-    'github_banner': True,
-    'description': 'GitHub App Tutorial using Python, gidgethub, and aiohttp',
-    'show_relbars': True,
+    "github_user": "mariatta",
+    "github_repo": "github-app-tutorial",
+    "github_type": "star",
+    "github_banner": True,
+    "description": "GitHub App Tutorial using Python, gidgethub, and aiohttp",
+    "show_relbars": True,
 }
 
 html_sidebars = {
-    '**': [
-        'about.html',
-        'sidebarlogo.html',
-        'localtoc.html',
-        'navigation.html',
-        'relations.html',
-        'searchbox.html',
+    "**": [
+        "about.html",
+        "sidebarlogo.html",
+        "localtoc.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
     ]
 }
 
-intersphinx_mapping = {'gidgethub': ('https://gidgethub.readthedocs.io/en/latest/', None),
-                       'exhale': ('https://exhale.readthedocs.io/en/latest/', None),
-                       }
+intersphinx_mapping = {
+    "gidgethub": ("https://gidgethub.readthedocs.io/en/latest/", None),
+}
